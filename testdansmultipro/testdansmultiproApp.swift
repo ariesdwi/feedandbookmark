1bd0286d525f1ff3072539c63e06b911cf754a5e
70001390 <ariesdwiprasetiyo4@gmail.com>
2025-08-09 10:13:43 +0700

    Initial Commit

---
+++ testdansmultipro/testdansmultiproApp.swift
@@ -0,0 +1,17 @@
+//
+//  testdansmultiproApp.swift
+//  testdansmultipro
+//
+//  Created by PT Siaga Abdi Utama on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct testdansmultiproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testdansmultiproTests/testdansmultiproTests.swift
@@ -0,0 +1,16 @@
+//
+//  testdansmultiproTests.swift
+//  testdansmultiproTests
+//
+//  Created by PT Siaga Abdi Utama on 09/08/25.
+//
+
+import Testing
+
+struct testdansmultiproTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
